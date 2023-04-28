fc8da2e458b43c8485bca1fc01ec1bc276cf6796
Kaden Bingham <kadenbingham03@gmail.com>
2023-04-28 12:38:26 -0400

    Creating edit post form

--- src/components/auth/editPostForm.jsx
+++ src/components/auth/editPostForm.jsx
@@ -1,10 +1,56 @@
+import { useState } from "react";
+const [name, setName] = useState("");
+const [description, setDescription] = useState("");
+const [price, setPrice] = useState("");
 
-
-export default function editPostForm(){
-    return 
+export default function editPostForm() {
+  return (
     <div>
-        <form>
-            <
-        </form>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <label className="label" htmlFor="newName">
+          New Name
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="input"
+          value={name}
+          placeholder="Buggati"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label className="label" htmlFor="description ">
+          Description
+        </label>
+        <input
+          type="description"
+          id="description"
+          value={description}
+          className="input"
+          placeholder="Super Funny Description"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <label className="label" htmlFor="price">
+          Price
+        </label>
+        <input
+          type="description"
+          id="price"
+          className="price"
+          placeHolder="$$$"
+          onChange={(e) => setPrice(e.target.value)}
+        ></input>
+        <button
+          type="submit"
+          className="button"
+          onClick={console.log("New Details", name, description, price)}
+        >
+          Login
+        </button>
+      </form>
     </div>
+  );
 }
